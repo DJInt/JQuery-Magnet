55a99d862cdc15083e3d30bfbe63b170c65b8714
jtannous <jerry.tannous@gmail.com>
2017-10-07 03:48:22 +0300

    Initial Commit

---
+++ jquery-magnet.js
@@ -0,0 +1,72 @@
+/**
+ * Developed By Jerry Tannous
+ * jerry.tannous@gmail.com
+ * Version 1.0
+ */
+(function ( $ ) {
+    $.fn.magnet = function( options ) {
+
+        var settings = $.extend({
+            after: null
+        }, options );
+
+        var $anchor;
+        var $after = null;
+        var isInTop = false;
+        var $elem = this;
+
+        $(document).ready(function(){
+            init();
+            adjustMagnetPosition(true);
+        });
+
+        $(window).on('scroll', function(){
+            adjustMagnetPosition(false);
+        });
+
+        return this;
+
+        function init()
+        {
+            // Insert Anchor
+            $anchor = $("<div class='jquery-magnet-anchor'></div>");
+            $anchor.insertBefore($elem);
+
+            if(settings.after !== null)
+                $after = $(settings.after);
+        }
+
+        function adjustMagnetPosition(force)
+        {
+            var offset = 0;
+            if($after !== null && $after !== 'undefined')
+            {
+                offset = $after.outerHeight();
+            }
+
+            var scrollTop     = $(window).scrollTop(),
+                elementOffset = $anchor.offset().top,
+                distance      = (elementOffset - scrollTop) - offset;
+
+
+            if(distance <= 0)
+            {
+                if(isInTop === false || force === true) {
+                    isInTop = true;
+                    $anchor.css({'height': $elem.outerHeight()});
+                    $elem.css({'position': 'fixed', 'top': offset});
+                }
+            }
+            else
+            {
+                if(isInTop === true || force === true) {
+                    isInTop = false;
+                    $anchor.css({'height': '0'});
+                    $elem.css({'position': 'static', 'top': 'auto'});
+                }
+            }
+        }
+
+    };
+
+}( jQuery ));
