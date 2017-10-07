c200cdb2cfff064d9ee2539b5d11ca65d2c19ca0
Jerry Tannous <jerry.tannous@gmail.com>
2017-10-07 22:13:28 +0300

    Bug fixes

--- jquery-magnet.js
+++ jquery-magnet.js
@@ -1,7 +1,7 @@
 /**
  * Developed By Jerry Tannous
  * jerry.tannous@gmail.com
- * Version 1.0.1
+ * Version 1.0.3
  */
 (function ( $ ) {
     $.fn.magnet = function( options ) {
@@ -10,17 +10,25 @@
             after: null
         }, options );
 
-        var $anchor;
+        var ogState = {
+            style: null,
+            width: null,
+        };
+
+        var $anchor = null;
         var $after = null;
         var isInTop = false;
         var $elem = this;
-        var oldCssPosition;
-        var oldTop;
-        var fixedWidth;
-        var oldWidth;
 
         $(document).ready(function(){
             init();
+            createAnchor();
+            adjustMagnetPosition(true);
+        });
+
+        $(window).on('resize', function(){
+            $elem.prop('style', ogState.style);
+            saveOGState();
             adjustMagnetPosition(true);
         });
 
@@ -30,13 +38,22 @@
 
         return this;
 
-        function init()
+        function createAnchor()
         {
             // Insert Anchor
             $anchor = $("<div class='jquery-magnet-anchor'></div>");
             $anchor.insertBefore($elem);
+        }
 
-            fixedWidth = $elem.outerWidth();
+        function saveOGState()
+        {
+            ogState.width = $elem.outerWidth();
+            ogState.style = $elem.prop('style');
+        }
+
+        function init()
+        {
+            saveOGState();
 
             if(settings.after !== null)
                 $after = $(settings.after);
@@ -57,13 +74,9 @@
             {
                 if(isInTop === false || force === true) { // We should stick to top
                     isInTop = true;
-                    oldWidth = $elem.outerWidth();
+                    saveOGState();
                     $anchor.css({'height': $elem.outerHeight()});
-                    oldCssPosition = $elem.css('position');
-                    oldTop = $elem.css('top');
-                    oldWidth = $elem.css('width');
-                    fixedWidth = $elem.outerWidth();
-                    $elem.css({'position': 'fixed', 'top': offset, 'width': fixedWidth});
+                    $elem.css({'position': 'fixed', 'top': offset, 'width': ogState.width});
                 }
             }
             else
@@ -71,7 +84,7 @@
                 if(isInTop === true || force === true) { // We should restore back to normal
                     isInTop = false;
                     $anchor.css({'height': '0'});
-                    $elem.css({'position': oldCssPosition, 'top': oldTop, 'width': oldWidth});
+                    $elem.prop('style', ogState.style);
                 }
             }
         }
